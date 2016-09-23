bc8a10a329f28d99b2e6729a70a43d2d850b9e54
prajwal051290 <pk.kondawar@gmail.com>
2016-09-22 22:05:03 -0700

    Invert Binary Tree

---
+++ InvertBinaryTree.java
@@ -0,0 +1,26 @@
+/*Invert a binary tree.
+
+Logic:
+Recursion..
+Keep inverting from bottom of tree, ultimately whole tree will be inverted*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null)
+            return root;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.right = left;
+        root.left = right;
+        return root;
+    }
+}
