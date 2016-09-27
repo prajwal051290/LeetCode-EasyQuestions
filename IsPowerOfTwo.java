6812342cc2f26389cc07dda22ce45895b55038e8
prajwal051290 <pk.kondawar@gmail.com>
2016-09-26 23:01:27 -0700

    Is power of 2

---
+++ IsPowerOfTwo.java
@@ -0,0 +1,15 @@
+/*Given an integer, write a function to determine if it is a power of two*/
+
+/*
+ * Logic:
+ * Approach 1: keep taking mod till you get zero. if zero then true else false
+ * Approach 2: Bitwise &
+ * 
+ * Corner Case: Number must be greater than zero
+ */
+
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return (n>0) && ((n&(n-1)) == 0);
+    }
+}
